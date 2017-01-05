be09f24b7e44aa483ff05539c0c2ab21794a6a1e
ZAWAR\Zawar Imam Qureshi <qureshizawar@gmail.com>
2017-01-05 15:19:34 +0500

    no message

--- RunSimulateCamera.m
+++ RunSimulateCamera.m
@@ -3,10 +3,31 @@
 
 
 % Construct a Camera
-[ KMatrix , CameraHeight , CameraWidth ] = BuildCamera ;
+%[ KMatrix , CameraHeight , CameraWidth ] = BuildCamera ;
+ChipWidth =  [200];
+CameraWidth =  [200];
+ChipHeight =  [300];
+CameraHeight =  [300];
+FocalLength = [1.0];
+PixelWidth = [0.0001];
+PixelHeight = [0.0001];
+Skewness = [-0.1];
+P_u = [0.25];
+P_v = [0.25];
+
+Parameters = [ ChipWidth , ChipHeight , ...
+    FocalLength , PixelWidth , PixelHeight ,...
+    Skewness ,P_u , P_v];
+
+KMatrix = SingleVectorCameraModel ( Parameters );
 
 % Construct an object in its own frame
-Cube = BuildCube ;
+%Cube = BuildCube ;
+ObjectLines = [
+ 0 1 1 0 0 0 1 1
+ 0 0 1 1 1 0 0 1
+ 0 0 0 0 1 1 1 1
+ 0 0 0 0 0 0 0 1];
 
 % Position the object in space
 T_ow = PositionObject ;
@@ -15,4 +36,4 @@ T_ow = PositionObject ;
 T_cw = PositionCamera ( T_ow );
 
 % Look at what we have
-ViewCamera (Cube ,T_ow , KMatrix , CameraHeight , CameraWidth , T_cw )
+ViewCamera (ObjectLines ,T_ow , KMatrix , CameraHeight , CameraWidth , T_cw )
